f5b854d199badf8b2ebb6f630ff1867a5cc08b58
Skeo <skeo051600@gmail.com>
2024-01-08 21:07:32 -0600

    Add intro.js component and implemented in index.js

---
+++ pages/Intro.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Intro() {
+  return (
+    <div
+      className="text-center d-flex flex-column justify-content-center align-content-center"
+      style={{
+        height: '30vh',
+        padding: '30px',
+        maxWidth: '400px',
+        margin: '0 auto',
+      }}
+    >
+      <h1>joke generator!! hehe ahahahaha AHAHAAA</h1>
+
+    </div>
+  );
+}
